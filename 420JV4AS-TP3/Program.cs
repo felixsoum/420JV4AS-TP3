db1b982d97cdc1feef57912d6e0d58e4c6b4ea1f
Felix Soumpholphakdy <felix@felixsoum.com>
2018-02-07 00:47:30 -0500

    Add project

---
+++ 420JV4AS-TP3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Nom ici
+
+using System;
+
+namespace _420JV4AS_TP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Question1a(7));
+            //Console.WriteLine(Question1a(20));
+            //Console.WriteLine(Question1b("bonk"));
+            //Console.WriteLine(Question1b("alucard"));
+            //Print(Question2a(Room.CreateDungeon(), 2));
+            //Print(Question2a(Room.CreateDungeon(), 0));
+            //Print(Question2b(Room.CreateDungeon()));
+            //Print(Question2c(Room.CreateDungeon()));
+            Console.Read();
+        }
+
+        static int Question1a(int n)
+        {
+            return 0;
+        }
+
+        static string Question1b(string s)
+        {
+            return s;
+        }
+
+        static Room Question2a(Room startingRoom, int n)
+        {
+            return startingRoom;
+        }
+
+        static Room Question2b(Room startingRoom)
+        {
+            return startingRoom;
+        }
+
+        static Room Question2c(Room startingRoom)
+        {
+            return startingRoom;
+        }
+
+        static void Print(Room startingRoom)
+        {
+            for (Room r = startingRoom; r != null; r = r.next)
+            {
+                Console.Write("-[{0}]", r.name);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 420JV4AS-TP3/Room.cs
@@ -0,0 +1,30 @@
+﻿namespace _420JV4AS_TP3
+{
+    class Room
+    {
+        public string name;
+        public Room previous;
+        public Room next;
+
+        public Room(string name) : this(name, null, null) {}
+
+        public Room(string name, Room next) : this(name, null, next) {}
+
+        public Room(string name, Room previous, Room next)
+        {
+            this.name = name;
+            this.previous = previous;
+            this.next = next;
+        }
+
+        public static Room CreateDungeon()
+        {
+            Room room = new Room("A", new Room("B", new Room("C", new Room("D", new Room("E")))));
+            for (Room r = room; r.next != null; r = r.next)
+            {
+                r.next.previous = r;
+            }
+            return room;
+        }
+    }
+}
